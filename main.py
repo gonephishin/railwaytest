1d0317b7c241a1ad37eb9cebe2d42a1d9ff0e410
stinky <stinky@stinky-mbp.lan>
2022-10-19 23:19:09 -0500

    fucking around and finding out

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
-
+app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
 
 @app.route('/')
 def index():
